79a49f039977651c04f2be277ebc960ea2125ebf
Matteo Pierro <pierromatteo@gmail.com>
2018-11-10 14:38:33 +0100

    Initial commit from Create React App

--- src/board.js
+++ src/board.js
@@ -8,11 +8,11 @@ export default class Board extends React.Component {
     }
 
     render() {
+        const status = 'Next player: X';
+
         return (
             <div>
-                <div className="status">
-                ???
-                </div>
+                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.addSquare()}
                     {this.addSquare()}
