7bebd1995715c971d87136bc4e0464f23ebb0e52
Matteo Pierro <pierromatteo@gmail.com>
2018-11-10 19:32:15 +0100

    removes duplication from tests

--- src/__tests__/game-test.js
+++ src/__tests__/game-test.js
@@ -6,35 +6,41 @@ import Adapter from 'enzyme-adapter-react-16';
 enzyme.configure({ adapter: new Adapter() });
 
 describe('Game', () => {
-    it('should start with X', () => {
-        const game = shallow(<Game />);
+    let game;
 
-        expect(game.state().currentPlayer).toBe('X');
+    beforeEach(() => {
+        game = shallow(<Game />);
+    })
+
+    it('should start with X', () => {
+        assertCurrentPlayer('X');
     });
 
     it('should allow O to play after X', () => {
-        const game = shallow(<Game />);
+        takeSquare();
 
-        game.instance().takeSquare();
-
-        expect(game.state().currentPlayer).toBe('O');
+        assertCurrentPlayer('O');
     });
 
     it('should alternate players', () => {
-        const game = shallow(<Game />);
+        takeSquare();
+        takeSquare();
 
-        game.instance().takeSquare();
-        game.instance().takeSquare();
-
-        expect(game.state().currentPlayer).toBe('X');
+        assertCurrentPlayer('X');
     });
 
     it('should notify the player to the Board', () => {
-        const game = shallow(<Game />);
-        
         const board = game.find(Board);
 
         expect(board.props().player).toBe('X')
     });
+
+    function takeSquare() {
+        game.instance().takeSquare();
+    }
+
+    function assertCurrentPlayer(player) {
+        expect(game.state().currentPlayer).toBe(player);
+    }
 });
 
